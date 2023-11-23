b32686fb7b2094baf51df8d880b8e8d5f54833f5
DShamak <dmitry.shamak.dev@gmail.com>
2023-11-23 12:08:04 +0300

    Update script domain

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -4,7 +4,7 @@ export default function GamePage({ id }) {
   return <main>
     <h1>Game Page #{id}</h1>
 
-    <script defer src={`http://localhost:3001/bidon.js?company=23ed84&player=${playerId}`}></script>
+    <script defer src={`https://next-ssr-sp4k.onrender.com/bidon.js?company=23ed84&player=${playerId}`}></script>
   </main>
 }
 
