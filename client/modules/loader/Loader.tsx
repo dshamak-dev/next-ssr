10f13caec62ae7177b1d0e68b9dd6b2f5389d490
DShamak <dmitry.shamak.dev@gmail.com>
2023-12-29 16:00:46 +0400

    Update Loader styles

--- client/modules/loader/Loader.tsx
+++ client/modules/loader/Loader.tsx
@@ -1,5 +1,12 @@
 import React, { PropsWithChildren } from "react";
+import styles from "./Loader.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch, faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export const Loader: React.FC<PropsWithChildren> = () => {
-  return <div>loading..</div>;
+  return (
+    <div className={styles.loader} title="loading..">
+      <FontAwesomeIcon icon={faSpinner} className={styles.icon} />
+    </div>
+  );
 };
